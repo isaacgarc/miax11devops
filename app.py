c8cf0a1f3d8f6b10453900f4ac6f75bd7ebeeca9
isaacgarc <isaac.garcia@argenconsulting.net>
2023-10-13 15:39:21 +0200

    1

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import datetime
+#pip install fsspec s3fs
 def handler(event,context):
-    df= pd.DataFrame([1,1,2,2])
-    print(df,'3')
+    df_raw= pd.read_csv('s3://miaxdevops/market_data.txt',sep=';')
+    df_raw=df_raw.loc[:,['FECHA','VALOR','PRECIO','VOLUMEN','HORA']]
+    print(df_raw)
+    df_raw = df_raw[df_raw.VALOR == 'SAN']
+    date = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    out_file_path = f's3://miaxdevops//market_data_{date}.txt'
+    df_raw = df_raw.to_csv(out_file_path)
+    print(f"out file created {out_file_path}")
     return 'Hola'
+handler([],[])
